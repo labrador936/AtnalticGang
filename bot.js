c55a550f2adbcc88abe62f1deca6208c8dc81378
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-08 16:02:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -79,22 +79,32 @@ client.on('message', async message => {
         });
     } 
  });
+client.on('message', message => {
 
-
-
- client.on('message', msg =>{
-    let message=msg;
-    if(message.content.startsWith("bc")){
-        var args = message.content.split(' ').slice(1).join(' ');
-    msg.guild.members.forEach(m=>{
-        m.send(args.replace(/[user]/g,m)).catch();
-    if(message.attachments.first()){
-m.sendFile(message.attachments.first().url).catch();
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == 'bc') {
+    if (!args[1]) {
+message.channel.send("**bc <message>**");
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField('» Server :', `${message.guild.name}`)
+            .addField('» Sender : ', `${message.author.username}#${message.author.discriminator}`)
+            .addField('» Message : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
     }
-    })    ;
+    } else {
+        return;
     }
 });
-
 
 
 
