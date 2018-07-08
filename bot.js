98dc0bbcef2c67f390910cfdd426354515dccba2
Ahmedbennasser <40671525+Ahmedbennasser@users.noreply.github.com>
2018-07-08 16:04:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = 'monster'
 
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
